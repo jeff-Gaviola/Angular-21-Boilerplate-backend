4a2ce212c4f35a946ae04a7807c23e5956b44028
jeff gaviola <jeffreygaviola95@gmail.comm>
2026-05-13 13:18:16 +0800

    fix swagger

--- _helpers/swagger.ts
+++ _helpers/swagger.ts
@@ -3,9 +3,11 @@ import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
 
 import path from 'path';
+import fs from 'fs';
 
 const router = express.Router();
-const swaggerDocument = YAML.load(path.join(process.cwd(), 'swagger.yaml'));
+const swaggerYaml = fs.readFileSync(path.join(__dirname, '../swagger.yaml'), 'utf8');
+const swaggerDocument = YAML.parse(swaggerYaml);
 
 router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
